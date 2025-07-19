8cc6a6fa926653a800e0c970517c51396908c22a
stranger156 <3426464117@qq.com>
2025-07-19 10:46:16 +0800

    添加修改

--- src/views/courseMessage.vue
+++ src/views/courseMessage.vue
@@ -3,7 +3,16 @@
 </template>
 
 <script lang="ts" setup >
-
+defineProps({
+  courseId: {
+    type: [String, Number],
+    required: true
+  },
+  teacherId: {
+    type: [String, Number],
+    required: true
+  }
+})
 </script>
 
 <style scoped>
